2f3b8ae3c31f0db5a4e4e5a7cbaba7365b4004e1
Brittni Rohrer <brittnirohrer@Brittnis-MacBook-Air.local>
2024-06-06 18:16:08 -0400

    add js file and image onclick

---
+++ adopt-a-dog.js
@@ -0,0 +1,48 @@
+function dogInfoMurph() {
+    let doggyText = document.getElementById('murphy');
+    alert(doggyText.innerText);
+}
+function dogInfoPop() {
+    let doggyText = document.getElementById('poppy');
+    alert(doggyText.innerText);
+}
+function dogInfoJack() {
+    let doggyText = document.getElementById('jack');
+    alert(doggyText.innerText);
+}
+function dogInfoDuffy() {
+    let doggyText = document.getElementById('duffy');
+    alert(doggyText.innerText);
+}
+function dogInfoLucas() {
+    let doggyText = document.getElementById('lucas');
+    alert(doggyText.innerText);
+}
+function dogInfoJake() {
+    let doggyText = document.getElementById('jake');
+    alert(doggyText.innerText);
+}
+function dogInfoAngus() {
+    let doggyText = document.getElementById('angus');
+    alert(doggyText.innerText);
+}
+function dogInfoVio() {
+    let doggyText = document.getElementById('vio');
+    alert(doggyText.innerText);
+}
+function dogInfoPip() {
+    let doggyText = document.getElementById('pip');
+    alert(doggyText.innerText);
+}
+function dogInfoMax() {
+    let doggyText = document.getElementById('max');
+    alert(doggyText.innerText);
+}
+function dogInfoLuna() {
+    let doggyText = document.getElementById('luna');
+    alert(doggyText.innerText);
+}
+function dogInfoStella() {
+    let doggyText = document.getElementById('stella');
+    alert(doggyText.innerText);
+}
