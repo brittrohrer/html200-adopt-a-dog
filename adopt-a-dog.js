c3ab67596dfd1011a3fc34ea86739ef9cb932d50
Brittni Rohrer <brittnirohrer@Brittnis-MacBook-Air.local>
2024-06-07 00:12:38 -0400

    added clickcounter function

--- adopt-a-dog.js
+++ adopt-a-dog.js
@@ -1,3 +1,13 @@
 function dogInfo(name, breed, fee) {
     alert('Name: ' + name + ', Breed: ' + breed + ', Adoption Fee: $' + fee);
 }
+
+let count = 0;
+total = 0;
+//output = document.getElementById('output')
+function countClicks() {
+    count = count + 1;
+    total = count * 123.45;
+    //output.innerHTML = '$' + total;
+    alert('New Total: $' + total);
+}
