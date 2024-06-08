52672bf7a39d18679811239a69013a10dcb17631
Brittni Rohrer <brittnirohrer@Brittnis-MacBook-Air.local>
2024-06-08 15:37:58 -0400

    trying a different way cost and quantity

--- adopt-a-dog.js
+++ adopt-a-dog.js
@@ -1,13 +1,18 @@
 function dogInfo(name, breed, fee) {
-    alert('Name: ' + name + ', Breed: ' + breed + ', Adoption Fee: $' + fee);
+    alert('Name: ' + name + '\n' + 'Breed: '  + breed + '\n' + 'Adoption Fee: $' + fee);
 }
 
-let count = 0;
-total = 0;
-//output = document.getElementById('output')
-function countClicks() {
-    count = count + 1;
-    total = count * 123.45;
-    //output.innerHTML = '$' + total;
-    alert('New Total: $' + total);
+// let count = 0;
+// total = 0;
+// price = 123.45;
+// function adoptCost() {
+//     count = count + 1;
+//     total = count * price;
+//     alert('New Total: $' + total);
+// }
+
+let cost = 0;
+function adoptCost(price, quantity) {
+    cost = price  * quantity;
+    alert('Total = $' + cost);
 }
