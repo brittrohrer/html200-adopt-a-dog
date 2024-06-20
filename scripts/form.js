b9a1cfd7bff076cb0c8fb2b192a419a7a97ded9c
Brittni Rohrer <brittnirohrer@Brittnis-MacBook-Air.local>
2024-06-19 22:00:58 -0400

    added console log of inputs

--- scripts/form.js
+++ scripts/form.js
@@ -4,5 +4,26 @@ form.addEventListener('submit', submitButton);
 function submitButton(e) {
     alert('Thank You. The form information has been recieved.');
     e.preventDefault();
-}
     
+}
+
+let inputsText = document.querySelectorAll('.form-text-input');
+let inputsRadio = document.querySelectorAll('.form-input-radio');
+let inputsCheckbox = document.querySelectorAll('.form-input-checkbox');
+function inputsValue() {
+    for (i = 0; i < inputsText.length; i++) {
+        console.log(inputsText[i].value);
+    }
+    for (i = 0; i < inputsRadio.length; i++) {
+        if (inputsRadio[i].checked) {
+            let radioValue = inputsRadio[i].value;
+            console.log(`Preferred Method of Contact: ${radioValue}`);
+        } 
+    }
+    for (i = 0; i < inputsCheckbox.length; i++) {
+        if (inputsCheckbox[i].checked) {
+            let checkboxValue = inputsCheckbox[i].value;
+            console.log(`Recieve emails about: ${checkboxValue}`);
+        } 
+    }
+}
