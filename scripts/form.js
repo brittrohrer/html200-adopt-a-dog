931a84c21aea27b37844837a97ba7147a3437642
Brittni Rohrer <brittnirohrer@Brittnis-MacBook-Air.local>
2024-06-21 18:39:37 -0400

    removed extra function

--- scripts/form.js
+++ scripts/form.js
@@ -11,23 +11,21 @@ function submitButton(e) {
     let inputsId = document.querySelectorAll('.form-text-input');
     let inputsRadio = document.querySelectorAll('.form-input-radio');
     let inputsCheckbox = document.querySelectorAll('.form-input-checkbox');
-    function inputsValue() {
-        for (i = 0; i < inputsText.length; i++) {
-            console.log(`${inputsId[i].id}:  ${inputsText[i].value}`);
-        }
-        for (i = 0; i < inputsRadio.length; i++) {
-            if (inputsRadio[i].checked) {
-                let radioValue = inputsRadio[i].value;
-                console.log(`Preferred Method of Contact: ${radioValue}`);
-            } 
-        }
-        for (i = 0; i < inputsCheckbox.length; i++) {
-            if (inputsCheckbox[i].checked) {
-                let checkboxValue = inputsCheckbox[i].value;
-                console.log(`Recieve emails about: ${checkboxValue}`);
-            } 
-        }
+    for (i = 0; i < inputsText.length; i++) {
+        console.log(`${inputsId[i].id}:  ${inputsText[i].value}`);
     }
+    for (i = 0; i < inputsRadio.length; i++) {
+        if (inputsRadio[i].checked) {
+            let radioValue = inputsRadio[i].value;
+            console.log(`Preferred Method of Contact: ${radioValue}`);
+        } 
+    }
+    for (i = 0; i < inputsCheckbox.length; i++) {
+        if (inputsCheckbox[i].checked) {
+            let checkboxValue = inputsCheckbox[i].value;
+            console.log(`Recieve emails about: ${checkboxValue}`);
+        } 
+    }   
 }
 
 
